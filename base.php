7122b9ca9f44904795620102bdff4780260ab1a4
Wendell P. Barreto <wendellp.barreto@gmail.com>
2017-04-26 10:31:03 -0300

    Update base.php

--- base.php
+++ base.php
@@ -4,26 +4,41 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 
-  <?php get_template_part('templates/head'); ?>
+<link rel="stylesheet" href="<?php echo Roots\Worth\Assets\bower_components_path('sweetalert2/dist/sweetalert2.min.css') ?>">
 
-  <body <?php body_class(); ?>>
+<?php get_template_part('templates/head'); ?>
+
+<body <?php body_class(); ?>>
 
     <!--[if IE]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'worth'); ?>
       </div>
-    <![endif]-->
+      <![endif]-->
 
-    <?php do_action('get_header') ?>
-    <?php get_template_part('templates/header') ?>
+      <?php do_action('get_header') ?>
+      <?php get_template_part('templates/header') ?>
 
-    <div class="wrap" role="document">
-      <?php include Wrapper\template_path(); ?>
-    </div>
+      <div class="wrap" role="document">
+        <?php include Wrapper\template_path(); ?>
+      </div>
 
-    <?php do_action('get_footer') ?>
-    <?php get_template_part('templates/footer') ?>
-    <?php wp_footer() ?>
+      <?php do_action('get_footer') ?>
+      <?php get_template_part('templates/footer') ?>
 
-  </body>
-</html>
+      <script src="<?php echo Roots\Worth\Assets\bower_components_path('jquery/dist/jquery.min.js') ?>"></script>
+
+      <script src="<?php echo Roots\Worth\Assets\bower_components_path('tether/dist/js/tether.js') ?>"></script>
+
+      <script src="<?php echo Roots\Worth\Assets\bower_components_path('bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+
+      <script src="<?php echo Roots\Worth\Assets\bower_components_path('sweetalert2/dist/sweetalert2.min.js') ?>"></script>
+
+      <script src="<?php echo Roots\Worth\Assets\asset_path('scripts/all.js') ?>"></script>
+
+      <script src="https://maps.googleapis.com/maps/api/js?key=[KEY]&callback=initMap"
+      async defer></script>
+
+      <!--<?php wp_footer() ?>-->
+    </body>
+    </html>
